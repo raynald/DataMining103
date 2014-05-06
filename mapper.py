c0d3a083a0097b6fff7b2c53f8682d5474bed161
Lei Zhong <lzhong@slabhg117.ethz.ch>
2014-05-06 17:54:42 +0200

    ddd

--- mapper.py
+++ mapper.py
@@ -2,39 +2,49 @@
 
 import numpy as np
 import sys
+#from scipy.spatial import Voronoi
 
 k = 200
 dim = 750
 count = 0
+csize = 2000
 #eta = 0.1
+Data = []
+w = [0 for j in range(csize)]
 
 myu = [[0 for j in range(dim)] for i in range(k)]
 
-def find(Element):
-    Ans = -1
-    Ansj = 0
-    for i in range(0, k, 1):
-        Sum = 0
-        for j in range(0, dim, 1):
-            Sum += (myu[i][j] - Element[j]) * (myu[i][j] - Element[j])
-        if Ans == -1 or Sum < Ans:
-            Ans = Sum
-            Ansj = i
-    return Ansj
+def coresets(D):
+    B = []
+    D_p = D
+    sample = []
+    #while len(D_p) != 0:
+    for i in range(csize):
+        tmp = np.random.random_integers(len(D)-1)
+        sample += [tmp]
+    for i in range(len(D)):
+        maxh = -1
+        for j in range(csize):
+            tmp = np.sum(np.square(np.subtract(np.array(D[i]),np.array(D[sample[j]]))))
+            if tmp < maxh or maxh == -1:
+                maxh = tmp
+                ans = j
+        w[ans] = w[ans] + 1
+    for i in range(csize):
+        print "%d\t%d\t" % (np.random.random_integers(csize*csize), w[i]),
+        for j in range(dim-1):
+            print "%d" % D[sample[i]][j],
+        print "%d" % D[sample[i]][dim-1]
 
 if __name__ == "__main__":
     np.random.seed(seed=42)
 
-    data = np.load("arr_0.npy")
+    data = np.load("/tmp/arr_0.npy")
 
-    for nu in range(0, 30, 1):#len(data),1):
+    for nu in range(0, 10000,1):#len(data),1):
         line = data[nu]
         #x = np.fromstring(line, sep = " ")
         x = line
         count = count + 1
-        c = find(x)
-        for i in range(0, dim, 1):
-            myu[c][i] = myu[c][i] + 1.0 / count * (x[i] - myu[c][i])
-    for i in range(0, dim, 1):
-        for j in range(0, k, 1):
-            print "%d\t%d\t%d" % (j, i, myu[j][i])
+        Data = Data + [x]
+    coresets(Data)
--- reducer.py
+++ reducer.py
@@ -5,18 +5,36 @@ import numpy as np
 
 k = 200
 dim = 750
-le = [[0 for j in range(dim)] for i in range(k)]
-d = [[0 for j in range(dim)] for i in range(k)]
+csize = 2000
+myu = [[0 for j in range(dim)] for i in range(k)]
+count = 0
+x = []
+
+
+def find(Element):
+    Ans = -1
+    Ansj = 0
+    for i in range(0, k, 1):
+        Sum = 0
+        for j in range(0, dim, 1):
+            Sum += (myu[i][j] - int(Element[j])) * (myu[i][j] - int(Element[j]))
+        if Ans == -1 or Sum < Ans:
+            Ans = Sum
+            Ansj = i
+    return Ansj
 
 if __name__ == "__main__":
     for line in sys.stdin:
         line = line.strip()
-        key1, key2, myu = np.fromstring(line, sep = "\t")
+        key1, key2 ,feature = line.split("\t")
+        x = feature.split()
         key1 = int(key1)
         key2 = int(key2)
-        le[key1][key2] = le[key1][key2] + 1
-        d[key1][key2] = d[key1][key2] + float(myu)
+        c = find(x)
+        for i in range(dim):
+            myu[c][i] = count / (count+int(key2)) * myu[c][i] + int(key2) / (count+int(key2)) * int(x[i])
+            count += int(key2)
     for i in range(0, k, 1):
         for j in range(0, dim, 1):
-            print "%f" % (d[i][j] * 1.0 / le[i][j]),
+            print "%f " % (myu[i][j]),
         print ""
