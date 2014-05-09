ed1170c339afe814e63ee9746c963407637213a4
Raynald Chung <raynaldchung@gmail.com>
2014-05-09 21:53:59 +0200

    sklearn

--- mapper_new.py
+++ mapper_new.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python2.7
 
+from time import time
 import numpy as np
 import sys
-#from scipy.spatial import Voronoi
+from sklearn.cluster import KMeans
 
 k = 200
 dim = 750
 count = 0
-csize = 600
+csize = 200
 #Data = []
 w = [0 for j in range(csize)]
 
 myu = [[0 for j in range(dim)] for i in range(k)]
 
 def coresets(D):
-    for i in range(csize):
-        print "%d\t%d\t" % (np.random.random_integers(csize*10), w[i]),
+    for i in range(k):
+        print "%d\t%d\t" % (np.random.random_integers(k*10), kk[i]),
         for j in range(dim-1):
             print "%f" % D[i][j],
         print "%f" % D[i][dim-1]
@@ -34,17 +35,19 @@ if __name__ == "__main__":
         #x = line
         count = count + 1
         sys.stderr.write(str(count)+'\t')
-        if count > csize:
-            D = np.tile(x, (csize,1))
-            tmp = np.square(np.subtract(Data, D))
-            s = tmp.sum(axis=1)
-            center = np.argmax(s)
-            w[center] = w[center] + 1
+        if count == 1:
+            Data = np.array(x)
         else:
-            #Data = np.concatenate((Data, x), axis=0)
-            if count == 1:
-                Data = x
-            else:
-                Data = np.vstack([Data,x])
-    coresets(Data)
-
+            Data = np.vstack([Data,x])
+    for i in range(k):
+        myu[i] = Data[i]
+    t0 = time()
+    km = KMeans(init = 'k-means++', n_clusters = k, precompute_distances = True, max_iter = 10)
+    km.fit(Data)
+    myu = np.array(km.cluster_centers_)
+    lab = np.array(km.labels_)
+    kk = [0 for j in range(k)]
+    for i in range(len(Data)):
+        kk[lab[i]] += 1
+    coresets(myu)
+    sys.stderr.write(str(time()-t0)+'\n')
