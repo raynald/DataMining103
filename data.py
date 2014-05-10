b3ae744a488d0caca40bc6570bc3c9a5e80da59e
Raynald Chung <raynaldchung@gmail.com>
2014-05-10 19:22:40 +0200

    new data.py

--- data.py
+++ data.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python2.7
 
 import numpy as np
+from sklearn.cluster import KMeans
 
 # Load the file stored in NPZ format
-data = np.load("training.npz")['arr_0']
+#data = np.load("training.npz")['arr_0']
+data = np.load("arr_0.npy")
+
+for i in range(1000):
+    for j in range(750):
+        print data[i][j],
+    print
 
 # To store the file in CSV format
-np.savetxt('training.csv', data)
+#np.savetxt('training.csv', data)
 
 # Load CSV file
-data = np.loadtxt('training.csv')
+#data = np.loadtxt('training.csv')
