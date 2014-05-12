302c019d70c666052e30867f003049910142d781
Raynald Chung <raynaldchung@gmail.com>
2014-05-12 20:56:11 +0200

    READ

--- README.md
+++ README.md
@@ -3,8 +3,12 @@ DataMining103
 
 第三個Projekt
 
-100 manual 20 + sklearn 40 =  fail
-50 sklearn + sklearn 20 = 749
-100 sklearn + sklearn 40 = fail
-100 sklearn + manual 40 = 748.1
+*50 + sklearn*
+*nothing + 2000pre+online*
+*nothing + online*
+*100 sklearn + minibatch = *749.99*
+*100 manual 20 + sklearn 40 =  fail*
+*50 sklearn + sklearn 20 = 749*
+*100 sklearn + sklearn 40 = fail*
+*100 sklearn + manual 40 = 748.1*
 
--- reducer_neu.py
+++ reducer_neu.py
@@ -9,7 +9,9 @@ myu = np.zeros(shape = (k, dim))
 nW = np.zeros(k)
 x = []
 cc = 0
-
+batch = 100
+tik = 0
+c = np.zeros(batch)
 
 def find(Element):
     xx = np.tile(Element,(k,1))
@@ -27,12 +29,16 @@ if __name__ == "__main__":
         key2 = int(key2)
         if cc < k:
             myu[cc] = x
-            nW[cc] = key2
+            nW[cc] = 1
         else:
-            c = find(x)
-            tmp = myu[c]
-            nW[c] += key2
-            myu[c] = np.add(tmp, np.multiply(np.subtract(x,tmp), key2 / nW[c]))
+            c[tik] = find(x)
+            if tik == batch - 1:
+                for j in range(batch):
+                    tmp = myu[c[j]]
+                    nW[c[j]] += 1
+                    myu[c[j]] = np.add(tmp, np.multiply(np.subtract(x,tmp), 1 / nW[c[j]]))
+                tik = -1
+            tik += 1
         cc += 1
     for i in range(0, k, 1):
         for j in range(0, dim, 1):
